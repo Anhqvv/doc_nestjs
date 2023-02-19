63b34c6bbe0605208515c4104f2a7bf457b5edac
Anhqvv <anhqvv2022@gmail.com>
2023-02-19 16:21:03 +0700

    first commit

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+
+import { Controller, Get, Req } from "@nestjs/common";
+import { Request } from "express";
+
+@Controller("cats")
+export class CatsController {
+	@Get()
+	findAll (): string {
+		return "This action returns all cats";
+	}
+}
---
+++ src/cats/cats.module.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+
+import { Module } from "@nestjs/common";
+import { CatsController } from "./cats.controller";
+
+@Module({
+	controllers: [CatsController],
+})
+export class CatsModule {}
